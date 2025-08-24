39183b3406e11d3d737b13bceb729955454ddf32
Joshan Lucmayan <joshanlucmayan838@gmail.com>
2025-08-24 18:56:50 +0800

    Add files via upload

---
+++ OOP/Abstract/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        Paper paper = new Paper("Cotton rag Paper", "Long", 5);
+        Pen pen = new Pen("Fountain Pen", "Black", "Fine", 1);
+        Pencil pencil = new Pencil("Charcoal Pencil", 7, 1);
+
+        pen.useFor();
+
+        System.out.println(pencil);
+        System.out.println(pen);
+        System.out.println(paper);
+    }
+}
