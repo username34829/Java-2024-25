f82aba0410a3fc8b1916273fcb3296ac47a6ae10
Joshan Lucmayan <joshanlucmayan838@gmail.com>
2025-08-30 22:14:43 +0800

    Add files via upload

---
+++ OOP/Wrapper_Classes/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.function.BinaryOperator;
+
+public class Main {
+    public static void main(String[] args){
+
+        Integer x = 10;
+        Double y = 10.30;
+        Character z = 'Z';
+        Boolean a = false;
+
+        String xx = x.toString();
+        //char i = xx.charAt(0);
+
+        //System.out.println(xx);
+        //System.out.println(Character.isLetter(i));
+        //System.out.println(Character.isDigit(i));
+
+
+    }
+}
